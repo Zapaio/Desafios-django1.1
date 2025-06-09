aab5d3127de0a4cb46088ce61467ab72e1caa266
Benjamin <bignaciosaavedramercado@gmail.com>
2025-06-08 20:05:28 -0400

    Agregado código de semana 2 con nuevas rutas

---
+++ principal/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.http import HttpResponse
+import random
+urlpatterns	=	[
+                path("hola/",views.hola),
+                path('',	views.saludo),
+                path("saludar_usuario/<str:nombre>/",views.saludar_usuario),
+                path("numero/", views.numero),]
+
+
--- principal/views.py
+++ principal/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
+
+from django.http import HttpResponse
+
+def saludar_usuario(request,nombre):
+    return HttpResponse(f"Hola {nombre}")
+
+def saludo(request,):
+    return HttpResponse("Hola desde django"
+    "<br>las rutas para visitar son: /hola/ , /numero/ "
+    "<br>Para ingresar al saludo de usuario debe escribir /saludar_usuario/(Su nombre)")
+
+def hola(request,):
+    return HttpResponse("Hola mundo")
+
+def numero(request,):
+    return HttpResponse(f"A continuacion, un numero aleatorio entre 0 y 100 =  {random.randint(0,100)}")
